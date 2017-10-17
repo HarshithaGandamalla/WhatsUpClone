ba4296f3b133d253f71fa9c3e81824e46e96efc5
JayakrishnaThota <jaygre@ufl.edu>
2017-10-17 13:46:37 -0400

    End to End Testing for Login Page

--- whatsappclone/e2e/app.e2e-spec.ts
+++ whatsappclone/e2e/app.e2e-spec.ts
@@ -1,14 +1,48 @@
-import { AppPage } from './app.po';
+import {  
+      browser, element, by
+  }
+  from 'protractor'; 
 
 describe('whatsappclone App', () => {
-  let page: AppPage;
+  // let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    // page = new AppPage();
+    browser.get('/');
+  });
+  it('Check whether the page appears or not', function() 
+  {
+    expect(browser.getTitle()).toBe("Whatsappclone");
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  //  it('should not allow the user to login with empty credentials', function() {
+        
+  //     // expect(browser.getTitle()).toBe("Whatsappclone");
+  //       element(by.id('username')).sendKeys('');
+  //       element(by.id('password')).sendKeys('');
+  //       element(by.id('email')).sendKeys('');
+  //       var login_check = element(by.id('login_button'));
+  //       login_check.click();
+  //       var timeoutInMilliseconds = 100000;
+  //       browser.wait(browser.ExpectedConditions.alertIsPresent(), timeoutInMilliseconds);
+  //       var alertDialog = browser.switchTo().alert();
+  //       expect(alertDialog.getText()).toEqual("Username can't be empty.");
+  // });
+
+
+  it('should not allow the user to login with empty credentials', function() {
+    
+  // expect(browser.getTitle()).toBe("Whatsappclone");
+      element(by.id('username')).sendKeys('jay');
+      element(by.id('email')).sendKeys('jay@ufl.edu');
+      element(by.id('password')).sendKeys('');
+      var create_check = element(by.id('create'));
+      create_check.click();
+      var timeoutInMilliseconds = 1000000;
+      browser.wait(browser.ExpectedConditions.alertIsPresent(), timeoutInMilliseconds);
+      var alertDialog = browser.switchTo().alert();
+      // expect(alertDialog.getText()).toEqual("Password can't be empty.");
+      expect(alertDialog.getText()).toEqual("alertttttttQ!HTTP fail.");
+      // {Alert text : HTTP fail.usernameee}
   });
 });
