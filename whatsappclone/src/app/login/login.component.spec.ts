742704f0f5a98f7314e242a47cad987377d442ce
HamsikaG <hamsikag15@gmail.com>
2017-10-17 13:10:39 -0400

    Update login.component.spec.ts

--- whatsappclone/src/app/login/login.component.spec.ts
+++ whatsappclone/src/app/login/login.component.spec.ts
@@ -1,5 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed , fakeAsync, tick} from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
+import { Router, RouterModule, ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from "@angular/router/testing";
+import {Location} from "@angular/common";
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +12,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ 
+        FormsModule,
+        HttpModule,
+        RouterTestingModule, // same any normal route config   
+       ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
