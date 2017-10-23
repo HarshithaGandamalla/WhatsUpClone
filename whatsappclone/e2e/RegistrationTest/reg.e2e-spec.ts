8237fe10c10ffc8a672fb31e907717ddc6f6bcad
Hamsika Gandamalla <hamsikag15@gmail.com>
2017-10-23 13:55:23 -0400

    Registration e2e test working, bug fixed

--- whatsappclone/e2e/RegistrationTest/reg.e2e-spec.ts
+++ whatsappclone/e2e/RegistrationTest/reg.e2e-spec.ts
@@ -1,9 +1,5 @@
 import { browser, element, by } from 'protractor'; 
-<<<<<<< HEAD
 import { RegPage } from './reg.po';
-=======
-import { RegPage } from './login.po';
->>>>>>> acd1381b36dea0f33ef2c7b79ba50bf917e2ea57
 
 describe('Reg App', () => {
 
@@ -13,32 +9,14 @@ describe('Reg App', () => {
      page = new RegPage();
   });
 
-  it('Check whether the page appears or not', function() 
-  {
-    page.navigateTo();
-<<<<<<< HEAD
-    var alert = browser.switchTo().alert();
-    expect(alert.getText()).toEqual("Username can't be empty.");
-=======
-    expect(page.getParagraphText()).toEqual('Please, login into your account');
-    
->>>>>>> acd1381b36dea0f33ef2c7b79ba50bf917e2ea57
-  });
-
-});
-
+ 
  
 
-<<<<<<< HEAD
+  it('should navigate to registration page', function() {
   
-=======
-  it('should not allow the user to register with empty credentials', function() {
-    var reg_check = element(by.id('btn_login'));
-    reg_check.click();
-    var timeout = 100000;
-    browser.wait(browser.ExpectedConditions.alertIsPresent(), timeout);
-    var alert = browser.switchTo().alert();
-    expect(alert.getText()).toEqual("Username can't be empty.");
+    // page.navigateTo();
+    // expect(page.getParagraphText()).toEqual('Please, login into your account');
+    expect(browser.switchTo().alert().getText()).toEqual("Username can't be empty.");
 });
 
->>>>>>> acd1381b36dea0f33ef2c7b79ba50bf917e2ea57
+});
