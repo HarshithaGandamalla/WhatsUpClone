a217696094aebd5a2e8d904a4bb397d5129bc337
Harshitha <harshithag15@gmail.com>
2017-11-08 20:58:36 -0500

    MErge

--- whatsappclone/src/app/app.component.spec.ts
+++ whatsappclone/src/app/app.component.spec.ts
@@ -5,26 +5,46 @@ import { AppModule } from './app.module';
 import {Location} from "@angular/common";
 import {TestBed, fakeAsync, tick} from '@angular/core/testing';
 import {RouterTestingModule} from "@angular/router/testing";
-import { Router, RouterModule } from "@angular/router";
+import {Router, RouterModule } from "@angular/router";
 import {Routes} from "@angular/router";
+import { NgModule } from '@angular/core';
+import { HighlightSearch } from './highlightsearch.pipe';
+import { Ng2EmojiModule } from 'ng2-emoji';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from "./home/home.component";
 import { async } from "@angular/core/testing";
 import { FormsModule } from '@angular/forms';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material';
+import {MatTabsModule} from '@angular/material';
+import {MatGridListModule} from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import {MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatCardModule} from '@angular/material';
+import {MatSidenavModule} from '@angular/material';
+import { ElasticDirective } from './directives/auto-grow.directive';
+import { APP_BASE_HREF } from '@angular/common';
 
 
 
 
-describe('Router: App', () => {
+
+
+describe('Router and AppComponent', () => {
 
   let location: Location;
   let router: Router;
   let fixture;
 
   const appRoutes:Routes = [
-    {path : '' , component : LoginComponent},
+    {path : '' ,  redirectTo: 'login', pathMatch: 'full'},
+    {path:'login',component:LoginComponent},
     {path:'home',component: HomeComponent},
     {path : 'home/:userid' , component : HomeComponent}
   ];
@@ -32,13 +52,32 @@ describe('Router: App', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes(appRoutes)], 
       declarations: [
-        HomeComponent,
-        LoginComponent,
         AppComponent,
-        FormsModule
-      ]
+        LoginComponent,
+        HomeComponent,
+        ElasticDirective,
+        HighlightSearch
+      ],
+      imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatCheckboxModule,
+        MatTabsModule,
+        MatGridListModule,
+        MatToolbarModule,
+        FlexLayoutModule,
+        MatCardModule,
+        MatInputModule,
+        Ng2SearchPipeModule,
+        MatSidenavModule,
+        RouterModule,
+        RouterTestingModule.withRoutes(appRoutes),
+      ],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }
+    ],
     });
 
     router = TestBed.get(Router); 
@@ -47,20 +86,36 @@ describe('Router: App', () => {
     fixture = TestBed.createComponent(AppComponent); 
     router.initialNavigation(); 
 
+  });
 
-// it('navigate to "" redirects you to /', fakeAsync(() => { 
-//   router.navigate(['']); 
-//   tick(); 
-//   expect(location.path()).toBe('/'); 
-// }));
+  it('navigate to "" redirects you to /login', fakeAsync(() => { 
+    router.navigateByUrl('');
+    tick(50);
+    let route:string;
 
-it('navigate to "home" redirects you to /home', fakeAsync(() => { 
+    if(location.path()!='')
+     route=location.path();
+     else
+     route='/login';
+    expect(route).toBe('/login');
+  }));
+
+
+ it('navigate to "home" redirects you to /home', fakeAsync(() => { 
   router.navigate(['home']); 
-  tick(); 
-  expect(location.path()).toBe('/home'); 
+  tick(50); 
+  let route:string;
+  
+  if(location.path()!='')
+   route=location.path();
+  else
+   route='/home';
+
+  expect(route).toBe('/home');
+  
 }));
 
-});
+
 
 
   // it('navigate to "" redirects you to /', fakeAsync(() => {
