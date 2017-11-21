0bf10c29ea05cec76ba0067d735721bf63a77c58
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-21 02:03:12 -0500

    Complete e2e test cases for login component

--- whatsappclone/e2e/LoginTest/login.e2e-spec.ts
+++ whatsappclone/e2e/LoginTest/login.e2e-spec.ts
@@ -9,47 +9,104 @@ describe('Login App', () => {
      page = new LoginPage();
   });
 
+  it('Check the page atitle', function() 
+  {
+    page.navigateTo();
+    expect(browser.getTitle()).toEqual('Whatsappclone');
+  });
+
   it('Check whether the page appears or not', function() 
   {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Please, login into your account');
-    
   });
 
-
-
-  it('should not allow the user to login with empty credentials', function() {
-        
-    expect(browser.getTitle()).toBe("Whatsappclone");
-    var login_check = element(by.id('submit_login'));
-    login_check.click();
-    var timeoutInMilliseconds = 100000;
-    browser.wait(browser.ExpectedConditions.alertIsPresent(), timeoutInMilliseconds);
-    var alertDialog = browser.switchTo().alert();
-    expect(alertDialog.getText()).toEqual("Username can't be empty.");
+  it('should display username input', () => {
+    const userNameInput = page.getLoginFormUsername();
+    expect(userNameInput).toBeTruthy();
   });
 
-  it('should fill in the login form', () => {
-    
-    browser.switchTo().alert().then(
-      function (alert) { alert.accept(); },
-    );   
+  it('should display email input', () => {
+    const emailInput = page.getLoginFormEmail();
+    expect(emailInput).toBeTruthy();
+  });
+
+  it('should display password input', () => {
+    const passwordInput = page.getLoginFormPassword();
+    expect(passwordInput).toBeTruthy();
+  });
+
+  it('should fill in the login form', () => 
+  {
     page.navigateTo();
-   // let email = page.getLoginFormEmail();
     let password = page.getLoginFormPassword();
     let username = page.getLoginFormUsername();
-    username.sendKeys('dimpu');
-   // email.sendKeys('hamsikag15@gmail.com');
-    password.sendKeys('dimpu');
-   // expect(email.getAttribute('value')).toEqual('hamsikag15@gmail.com');
-    expect(password.getAttribute('value')).toEqual('dimpu');
-    expect(username.getAttribute('value')).toEqual('dimpu');
-    
+    username.sendKeys('jay');
+    password.sendKeys('jayakrishna');   
     let btn = page.getLoginButton();
     btn.click();
-    expect(browser.getCurrentUrl()).toEqual('http://localhost:49152/home/5a04b05097749137b491e193');
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:49159/home/5a137b30a75e121758df3713');
     element(by.id('logout')).click();
+ });
 
+  it('should not allow the user to login with empty username', () => 
+  {
+    page.navigateTo();
+    let password = page.getLoginFormPassword();
+    let username = page.getLoginFormUsername();
+    username.sendKeys('');
+    password.sendKeys('jayakrishna');   
+    let btn = page.getLoginButton();
+    btn.click();
+    browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+    var alertDialog = browser.switchTo().alert();
+    expect(alertDialog.getText()).toEqual("Username can't be empty.");
+    alertDialog.dismiss();
+ });
+
+ it('should not allow the user to login with empty password', () => 
+ {
+   page.navigateTo();
+   let password = page.getLoginFormPassword();
+   let username = page.getLoginFormUsername();
+   username.sendKeys('jay');
+   password.sendKeys('');   
+   let btn = page.getLoginButton();
+   btn.click();
+   browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+   var alertDialog = browser.switchTo().alert();
+   expect(alertDialog.getText()).toEqual("Password can't be empty.");
+   alertDialog.dismiss();
+});
+
+it('should not allow unregistered users', () => 
+{
+  page.navigateTo();
+  let password = page.getLoginFormPassword();
+  let username = page.getLoginFormUsername();
+  username.sendKeys('unregistered');
+  password.sendKeys('unregistered');   
+  let btn = page.getLoginButton();
+  btn.click();
+  browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+  var alertDialog = browser.switchTo().alert();
+  expect(alertDialog.getText()).toEqual("User not found ,please register");
+  alertDialog.dismiss();
+});
+
+it('should not allow the user to login with incorrect credentials', () => 
+{
+  page.navigateTo();
+  let password = page.getLoginFormPassword();
+  let username = page.getLoginFormUsername();
+  username.sendKeys('jay');
+  password.sendKeys('abcd');   
+  let btn = page.getLoginButton();
+  btn.click();
+  browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+  var alertDialog = browser.switchTo().alert();
+  expect(alertDialog.getText()).toEqual("User not found ,please register");
+  alertDialog.dismiss();
 });
 
 });
