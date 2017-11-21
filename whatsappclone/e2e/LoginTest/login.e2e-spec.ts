f3e012fd27f8c02eb6f91711d4af82a835e9da72
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-21 02:20:02 -0500

    complete e2e test cases for login page
    Done with both login component and registration component for the login page

--- whatsappclone/e2e/LoginTest/login.e2e-spec.ts
+++ whatsappclone/e2e/LoginTest/login.e2e-spec.ts
@@ -109,4 +109,91 @@ it('should not allow the user to login with incorrect credentials', () =>
   alertDialog.dismiss();
 });
 
+it('should not allow the user to register with empty username', () => 
+{
+  page.navigateTo();
+  let password = page.getLoginFormPassword();
+  let username = page.getLoginFormUsername();
+  let email = page.getLoginFormEmail();
+  username.sendKeys('');
+  password.sendKeys('jayakrishna'); 
+  email.sendKeys('test1@gmail.com');   
+  let btn = page.getRegisterButton();
+  btn.click();
+  browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+  var alertDialog = browser.switchTo().alert();
+  expect(alertDialog.getText()).toEqual("Username can't be empty for registration.");
+  alertDialog.dismiss();
+});
+
+
+it('should not allow the user to register with empty password', () => 
+{
+  page.navigateTo();
+  let password = page.getLoginFormPassword();
+  let username = page.getLoginFormUsername();
+  let email = page.getLoginFormEmail();
+  username.sendKeys('test2');
+  password.sendKeys(''); 
+  email.sendKeys('test2@gmail.com');  
+  let btn = page.getRegisterButton();
+  btn.click();
+  browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+  var alertDialog = browser.switchTo().alert();
+  expect(alertDialog.getText()).toEqual("Password can't be empty for registration.");
+  alertDialog.dismiss();
+});
+
+it('should not allow the user to register with empty email', () => 
+{
+  page.navigateTo();
+  let password = page.getLoginFormPassword();
+  let username = page.getLoginFormUsername();
+  let email = page.getLoginFormEmail();
+  username.sendKeys('test3');
+  password.sendKeys('test3'); 
+  email.sendKeys('');  
+  let btn = page.getRegisterButton();
+  btn.click();
+  browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+  var alertDialog = browser.switchTo().alert();
+  expect(alertDialog.getText()).toEqual("Email can't be empty for registration.");
+  alertDialog.dismiss();
+});
+
+it('should not allow the already registered user', () => 
+{
+  page.navigateTo();
+  let password = page.getLoginFormPassword();
+  let username = page.getLoginFormUsername();
+  let email = page.getLoginFormEmail();
+  username.sendKeys('jay');
+  password.sendKeys('jayakrishna'); 
+  email.sendKeys('jay@gmail.com');  
+  let btn = page.getRegisterButton();
+  btn.click();
+  browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+  var alertDialog = browser.switchTo().alert();
+  expect(alertDialog.getText()).toEqual("There is already an existing account with the same username. Please choose a new unique username");
+  alertDialog.dismiss();
+});
+
+//For this to run successfully every time, we need to manually delete this e2e user from this database
+//Or else it'll be considered as duplicate user and hence 
+//We get an alert regarding this is a duplicate user
+//So after running this test
+//Remove the newly created e2e user from mlab database 
+it('should allow legit user', () => 
+{
+  page.navigateTo();
+  let password = page.getLoginFormPassword();
+  let username = page.getLoginFormUsername();
+  let email = page.getLoginFormEmail();
+  username.sendKeys('e2e');
+  password.sendKeys('e2e'); 
+  email.sendKeys('e2e@gmail.com');  
+  let btn = page.getRegisterButton();
+  btn.click();
+});
+
 });
