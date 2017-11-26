2e9eb3b05cbad3e1fa7061ffa052815a1d5bd02e
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-26 10:09:14 -0500

    Update home.po.ts

--- whatsappclone/e2e/homeTest/home.po.ts
+++ whatsappclone/e2e/homeTest/home.po.ts
@@ -2,10 +2,35 @@ import { browser, by, element } from 'protractor';
 
 export class HomePage {
   navigateTo() {
-    return browser.get('/home/59e503b3333b2d08d8edc3f0');
+    browser.get('/');
+    element(by.id('username')).sendKeys('jay');
+    element(by.id('password')).sendKeys('jayakrishna');    
+    element(by.id('email')).sendKeys('jay@gmail.com');
+    element(by.id('submit_login')).click();
+    browser.sleep(500);
   }
 
   getParagraphText() {
     return element(by.id('container')).getText();
+  }
+
+  getUserText(){
+    return element(by.className('jay')).getText();
+  }
+
+  getSettings(){
+    return element(by.className('dropdown-button waves-effect waves-light')).getText();
+  }
+
+  getLogout(){
+    return element(by.id('logout')); 
+  }
+
+  getOnline(){
+    return element(by.className('active'));
+  }
+
+  getOffline(){
+    return element(by.className('inactive'));
   }
 }
