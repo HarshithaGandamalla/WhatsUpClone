1fbae5c1ed4ab1f067742ed41b21879809cc8bf0
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-26 17:27:11 -0500

    some more cases

--- whatsappclone/e2e/homeTest/home.e2e-spec.ts
+++ whatsappclone/e2e/homeTest/home.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { HomePage } from './home.po';
-import { browser } from "protractor/built";
+import { browser,protractor,element,by } from "protractor/built";
 
 describe('whatsappclone App', () => {
   let page: HomePage;
@@ -64,5 +64,62 @@ describe('whatsappclone App', () => {
     page.getOnline().click();
   });
 
+  it('should not send a without recipient', () => 
+  {
+    page.navigateTo();
+    var chat = page.getChatFooter();
+    chat.sendKeys("Hi",protractor.Key.ENTER);
+    browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+    var alertDialog = browser.switchTo().alert();
+    expect(alertDialog.getText()).toEqual("Select a user or group to chat.");
+    alertDialog.dismiss();
+  });
+  
+  
+  it('should not register a group without group name', () => 
+  {
+    page.navigateTo();
+    var newg = element(by.id('group1'));
+    newg.click();
+    var input = element(by.id('newgroup'));
+    input.sendKeys("",protractor.Key.ENTER);
+    browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+    var alertDialog = browser.switchTo().alert();
+    expect(alertDialog.getText()).toEqual("Please enter group name");
+    alertDialog.dismiss();
+  });
+
+  it('should successfully update status', () => 
+  {
+    page.navigateTo();
+    var newg = element(by.id('status1'));
+    newg.click();
+    var input = element(by.id('newstatus'));
+    input.sendKeys("I can update status",protractor.Key.ENTER);
+    browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+    var alertDialog = browser.switchTo().alert();
+    expect(alertDialog.getText()).toEqual("Status updated Successfully");
+    alertDialog.dismiss();
+  });
+
+  it('should successfully find the search box', () => 
+  {
+    page.navigateTo();
+    var search = element(by.id('search-box'));
+    expect(search).toBeTruthy();
+  });
+
+  it('should successfully update status', () => 
+  {
+    page.navigateTo();
+    var newg = element(by.id('status1'));
+    newg.click();
+    var input = element(by.id('newstatus'));
+    input.sendKeys("I can update status",protractor.Key.ENTER);
+    browser.wait(browser.ExpectedConditions.alertIsPresent(), 5000);
+    var alertDialog = browser.switchTo().alert();
+    expect(alertDialog.getText()).toEqual("Status updated Successfully");
+    alertDialog.dismiss();
+  });
   
 });
