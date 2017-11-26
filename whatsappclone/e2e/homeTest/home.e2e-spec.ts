241dd456deec1dd594817dddae0281781f8ec187
Venkata Jaya Krishna Thota <jaygre@ufl.edu>
2017-11-26 10:56:49 -0500

    more e2e test cases

--- whatsappclone/e2e/homeTest/home.e2e-spec.ts
+++ whatsappclone/e2e/homeTest/home.e2e-spec.ts
@@ -20,6 +20,49 @@ describe('whatsappclone App', () => {
     expect(browser.getTitle()).toEqual('Whatsappclone');
   });
 
+  it('should have the correct userId', () => 
+  {
+    page.navigateTo();
+    expect(browser.getCurrentUrl()).toContain('5a137b30a75e121758df3713');
+  });
+
+  it('should have the correct username', () => 
+  {
+    page.navigateTo();
+    expect(page.getUserText()).toContain('jay');
+  });
+
+  it('should have user settings', () => 
+  {
+    page.navigateTo();
+    expect(page.getSettings()).toBe('Settings');
+  });
+
+  it('should logout when user asks it to', () => 
+  {
+    page.navigateTo();
+    var l = page.getLogout();
+    var EC = browser.ExpectedConditions;
+    l.click();  
+  });
+
+  it('should check all the offline users', () => 
+  {
+    page.navigateTo();
+    page.getOffline().click();
+  });
+
+  it('should check all the online users', () => 
+  {
+    page.navigateTo();
+    page.getOnline().click();
+  });
+
+  it('should check all the online users', () => 
+  {
+    page.navigateTo();
+    page.getOnline().click();
+  });
+
   
 });
-
